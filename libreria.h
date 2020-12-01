fa650241350992267cf9ccafa6f14a079a9a7162
Lucas Depetris <66499356+lucasdepetris19@users.noreply.github.com>
2020-12-01 01:40:00 -0300

    Se agrega libreria.h al repo

---
+++ libreria.h
@@ -0,0 +1,44 @@
+struct veterinario
+{
+	char ApeNom[60];
+	int matri;
+	int DNI;
+	char telef[25];
+};
+
+struct fecha
+{
+	int dd;
+	int mm;
+	int aa;
+};
+
+struct veterinario
+{
+	char ApeNom[60];
+	char Dom[60];
+	int DNI;
+	char Loc[60];
+	fecha fec;
+	float peso;
+	char telef[25];
+};
+
+struct fecha
+{
+	int dd;
+	int mm;
+	int aa;
+};
+
+struct veterinario
+{
+	int matri;
+	fecha fec;
+	int DNI;
+	char DetA[380];
+};
+
+void regisvet (){
+	printf("Registre el veterinario %s\n");
+}
