8e42123f42354f0cd828e9b8b8a5e7ee3e2953ca
Lucas Depetris <66499356+lucasdepetris19@users.noreply.github.com>
2020-12-01 02:51:57 -0300

    update regisventa en libreria.h

--- libreria.h
+++ libreria.h
@@ -13,7 +13,7 @@ struct fecha
 	int aa;
 };
 
-struct veterinario
+struct mascota
 {
 	char ApeNom[60];
 	char Dom[60];
@@ -33,20 +33,78 @@ struct fecha
 
 struct veterinario
 {
+	char ApeNom[60];
 	int matri;
-	fecha fec;
 	int DNI;
-	char DetA[380];
+	char telef[25];
 };
 
-void regiusuario(){
+void regiusuario()
+{
 	printf("Registre el usuario %s\n");
 }
 
-void regisvet (){
+void regisvet()
+{
+	// char nom[20], ape[20], carrera[30];
+	// int cantf = 0, b = 0;
+	// float prom = 0;
+	int b = 0;
+	veterinario vet;
+
+	FILE *fp;
+	fp = fopen("Veterinarios.dat", "a+b");
+
+	printf("Registro de Veterinario Nº%d", i + 1);
+	printf("\n------------------------------\n");
+
+	_flushall();
+	do
+	{
+		printf("Apellido y Nombre: ");
+		gets(vet.ApeNom);
+	} while (strlen(vet.ApeNom) == 0);
+	_flushall();
+
+	do
+	{
+		printf("Matricula: ");
+		scanf("%06d", &vet.matri);
+	} while (vet.matri >= 0 && vet.matri <= 999999);
+
+	do
+	{
+		printf("DNI: ");
+		scanf("%08d", &vet.DNI);
+	} while (vet.DNI >= 10000000 && vet.DNI <= 99999999);
+
+	_flushall();
+	do
+	{
+		printf("Télefono: ");
+		gets(vet.telef);
+		i = 0;
+		while (vet.telef[i] != '\0')
+		{
+			i++;
+			if (isdigit(vet.telef[i]) || vet.telef[i] == '+')
+				b = 1;
+		}
+
+	} while (strlen(vet.telef) == 0 || b == 1);
+	_flushall();
+
+	fprintf(fp, "%s;%06d;%08d;%s;\n",vet.ApeNom,vet.matri,vet.DNI,vet.telef);
+	
+	fclose(fp);
+	system("CLS");
+}
+
+void regisvet()
+{
 	printf("Registre el veterinario %s\n");
 }
 
-int menuPrincipal(){
+int menuPrincipal()
+{
 }
-
