0fc9ec5b3794abc42ef80f7ca0f0edac68b76866
Santiaguito <decanito1@hotmail.com>
2020-12-01 02:28:15 -0300

    Se agregan los menus para interfaz de usuario

---
+++ MENUS.cpp
@@ -0,0 +1,97 @@
+int menuPrincipal()
+{
+    int opc = 0;
+    system("CLS");
+    printf("\n\t*****************************************************\n");
+    printf("\t**     C O N S U L T O R I O  V E T R I N A R I O  **\n");
+    printf("\t*****************************************************\n");
+    printf("\t**                                                 **\n");
+    printf("\t**   1-> Iniciar Sesion                            **\n");
+    printf("\t**                                                 **\n");
+    printf("\t*****************************************************\n");
+    printf("\t**                                                 **\n");
+    printf("\t**   2-> Visualizar Lista de Espera de Turnos      **\n");
+    printf("\t**                                                 **\n");
+    printf("\t*****************************************************\n");
+    printf("\t**                                                 **\n");
+    printf("\t**   3-> Registrar Evolucion de la Mascota         **\n");
+    printf("\t**                                                 **\n");
+    printf("\t*****************************************************\n");
+    printf("\t**                                                 **\n");
+    printf("\t**   4-> S A L I R  del  S I S T E M A             **\n");
+    printf("\t**                                                 **\n");
+    printf("\t*****************************************************\n\n");
+    printf("\tSeleccione una Opción: ");
+    scanf("%d", &opc);
+ 
+    return opc; //retorna el número de opción seleccionada.
+}
+
+
+int menuPrincipal()
+{
+    int opc = 0;
+    system("CLS");
+    printf("\n\t*****************************************************\n");
+    printf("\t**  A S I S T E N T E   D E  V E T E R I N A R I O   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   1-> Iniciar Sesion                              **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   2-> Registrar Mascota                           **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   3-> Registrar Turno                             **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   4-> List. de Atenciones por Veterinario y fecha **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   5-> S A L I R  del  S I S T E M A               **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n\n");
+    printf("\tSeleccione una Opción: ");
+    scanf("%d", &opc);
+ 
+    return opc; //retorna el número de opción seleccionada.
+}
+
+
+int menuPrincipal()
+{
+    int opc = 0;
+    system("CLS");
+    printf("\n\t*****************************************************\n");
+    printf("\t**            A D M I N I S T R A C I O N            **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   1-> Registrar Veterinario                       **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   2-> Registrar Usuario Asistente                 **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   3-> Atenciones por Veterinarios                 **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   4-> Ranking de Veterinarios por Atenciones      **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n");
+    printf("\t**                                                   **\n");
+    printf("\t**   5-> S A L I R  del  S I S T E M A               **\n");
+    printf("\t**                                                   **\n");
+    printf("\t*******************************************************\n\n");
+    printf("\tSeleccione una Opción: ");
+    scanf("%d", &opc);
+ 
+    return opc; //retorna el número de opción seleccionada.
+}
+
