c79e1c1a81f0ed65146ab3de6702f4f5970d2324
Lucas Depetris <66499356+lucasdepetris19@users.noreply.github.com>
2020-12-12 22:19:03 -0300

    Crear mainvet.cpp y carpeta mains

---
+++ stage/mainvet.cpp
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <cstring>
+#include <ctype.h>
+#include <wchar.h>
+// #include<string.h>
+// #include<time.h>
+// #include<cmath>
+
+#include "libreria.h"
+#include "menus.h"
+
+main(){
+
+}
